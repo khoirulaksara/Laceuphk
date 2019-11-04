fb33671dc86736202a8c47752ef8205b38ab25bf
Jason Wong <jasonwong@vgt-hk.net>
2019-11-04 15:19:12 +0800

    [New] News feed Page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,50 @@
 import 'package:flutter/material.dart';
-import 'wp-api.dart';
+import './News.dart';
+import './Store.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyAppState extends State<MyApp> {
+  int _selectedPage = 0;
+  final _pageOptions = [
+    NewsPage(),
+    StorePage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('LaceupHK'),),
-      body: Container(
-        child: ,
-      )
+    return MaterialApp(
+      title: 'LaceupHK',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('LaceupHK'),
+        ),
+        body: _pageOptions[_selectedPage],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedPage,
+          onTap: (int index) {
+            setState(() {
+              _selectedPage = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text('News')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.store), title: Text('Store')),
+          ],
+        ),
+      ),
     );
   }
 }
